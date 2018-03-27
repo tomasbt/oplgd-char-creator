2e0dc14b4720c194791a7554d8e0391d16e10d66
Tomas BT <tomastrillingsgaard@gmail.com>
2018-03-27 22:35:22 +0200

    added minor stuff for character creation

--- ol_functions/character_functions.py
+++ ol_functions/character_functions.py
@@ -10,6 +10,49 @@ def create_character(character_dict):
     character = ol_classes.character(name)
 
     if 'race' in character_dict.keys():
-        1
+        character.race = character_dict["race"]
+
+    # player name 
+
+    # archtype
+
+    # level 
+
+    # experience
+
+    # description
+
+    # attributes
+
+    # feats
+
+    # equipment
+
+    # defences
+
+    # wealth
+    
+    # speed 
+
+    # hit points
+
+    # initiative
+
+    # perks
+
+    # flaws
+
+    # actions
 
     return character
+
+def create_character_guide():
+    # Initial dictionary
+    character_dict = {}
+
+    # Get name and race
+    character_dict["name"] = input("Character name: ")
+    character_dict["race"] = input("race: ")    
+
+    return create_character(character_dict)
+    
--- testing_script.py
+++ testing_script.py
@@ -3,14 +3,19 @@ import ol_classes as ol_c
 import ol_functions as ol_f
 
 
+
 if __name__ == '__main__':
     print("Starting testing script")
 
-    my_dict = {"name": "Karsten"}
 
-    tmp = ol_f.create_character(my_dict)
+    # name = input("please enter the name: ")
+    # my_dict = {"name": name}
+    tmp = ol_f.create_character_guide()
+    # tmp = ol_f.create_character(my_dict)
 
-    print(tmp.name)
+
+    print(tmp.name, tmp.race)
+
 
     
     
